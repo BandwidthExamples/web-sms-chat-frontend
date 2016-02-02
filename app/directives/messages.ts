912fa0f7fe8945e9eaeea3ee3cc086769465aa52
Andrey Belchikov <belchikovs@gmail.com>
2016-02-02 13:04:19 +0300

    sms chat became more pretty

--- app/directives/messages.ts
+++ app/directives/messages.ts
@@ -1,8 +1,24 @@
-import {Component, View} from "angular2/core";
+import {Component, View, ViewEncapsulation, Pipe, PipeTransform} from "angular2/core";
+import * as moment_ from 'moment';
+
+// under systemjs, moment is actually exported as the default export, so we account for that
+const moment = (<any>moment_)['default'] || moment_;
+
+
+@Pipe({
+  name: "fromNow"
+})
+class FromNowPipe implements PipeTransform {
+  transform(value: any, args: any[]) {
+    return moment(value).fromNow();
+  }
+}
+
 
 @Component({
   selector: "message",
   inputs: ["item"],
+  pipes: [FromNowPipe],
   templateUrl: "app/directives/message.html"
 })
 export class MessageView {}
@@ -11,7 +27,9 @@ export class MessageView {}
   selector: "messages",
   inputs: ["items"],
   templateUrl: "app/directives/messages.html",
-  directives: [MessageView]
+  directives: [MessageView],
+  styleUrls: ["styles/messages.css"],
+  encapsulation: ViewEncapsulation.Native
 })
 export class MessagesView {
 }
