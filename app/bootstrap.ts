e3ea3614a2de223bac2c2debac469c308d52dee5
Andrey Belchikov <belchikovs@gmail.com>
2016-02-12 08:37:59 +0000

    Updated to Angular2.beta6

--- app/bootstrap.ts
+++ app/bootstrap.ts
@@ -2,5 +2,5 @@ import {bootstrap}    from "angular2/platform/browser"
 import {SmsApp} from "./components/app"
 import {enableProdMode} from "angular2/core"
 
-//enableProdMode();
+enableProdMode();
 bootstrap(SmsApp);
